63b2b4ec2a55cd43853bb6cc9c62ec921b7f5d60
mdfahim123123 <mdf522201@gmail.com>
2025-09-09 23:38:29 +0600

    My final code is here

--- index.js
+++ index.js
@@ -5,16 +5,35 @@ const loadCategories = () => {
 };
 
 
+
+
+const removeActive = () =>{
+  const btns = document.querySelectorAll(".btn-btn")
+  btns.forEach((btn)=>btn.classList.remove("active"));
+};
+
+const cardDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayCardDetails(details.plants)
+};
+
+const displayCardDetails = (detels) =>{
+  console.log(detels)
+}
+
+
 const loadCard = (id) => {
 
 const url = ` https://openapi.programming-hero.com/api/category/${id}`;
 fetch(url)
 .then(res=>res.json())
 .then(card => {
-   
-   const clickBtn = document.getElementById(` btn-category-${id}`)
+   removeActive();
+   const clickBtn = document.getElementById(`btn-category-${id}`)
    console.log(clickBtn)
-
+clickBtn.classList.add("active");
    
     displayCard (card.plants)
 });
@@ -35,7 +54,7 @@ const displayCard = (cards) => {
       alt="" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">
+    <h2 class="card-title" onclick ="loadCardDetails${card.id}">
       "${card.name}"
      </h2>
     <p>"${card.description}"</p>
@@ -60,7 +79,7 @@ const displayCategories = (categories) => {
     for(let category of categories ){
 
         const btnDiv = document.createElement("div");
-         btnDiv.innerHTML = `<button id="btn-category-${category.id}" onclick="loadCard(${category.id})" class=" btn w-45 ">${category.category_name}</button>
+         btnDiv.innerHTML = `<button id="btn-category-${category.id}" onclick="loadCard(${category.id})" class=" btn w-45 btn-btn">${category.category_name}</button>
          `;
 
 
@@ -71,6 +90,22 @@ const displayCategories = (categories) => {
 }
 
 
-
-
 loadCategories()
+
+
+const loadAllCard = () => {
+
+const url = `https://openapi.programming-hero.com/api/plants`;
+fetch(url)
+.then(res=>res.json())
+.then(card => {
+  console.log(card)
+ 
+
+   
+    displayCard (card.plants)
+});
+
+};
+
+loadAllCard()
